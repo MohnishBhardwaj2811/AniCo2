aa61c6ed182e518eaa635fc8d70b151a08841366
MohnishBhardwaj2811 <160467618+MohnishBhardwaj2811@users.noreply.github.com>
2024-04-02 23:52:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1021,16 +1021,16 @@ def get_user_player(user_id):
         data = extract_embedded_Video(convert_player_url(Urls.Suffix + "/" + user_id,0))
         if data:
             data = data["Video Player Link"]
-            return redirect(data)
+            #return redirect(data)
             
-            #return player_html(data)
+            return player_html(data)
             #return css_player_html(data)
         else:
             data = extract_embedded_Video(convert_player_url(Urls.Suffix + "/" + user_id,1))
             data = data["Video Player Link"]
-            return redirect(data)
+            #return redirect(data)
             
-            #return player_html(data)
+            return player_html(data)
             #return css_player_html(data)
     except:
         return Error_html()
